e490fb8e15e398af589936c54b46dea079aa006d
Jakub Owczarzak <66114694+Jakub-Owczarzak@users.noreply.github.com>
2021-06-15 18:35:46 +0200

    Update LoginForm.js

--- blog/src/components/LoginForm/LoginForm.js
+++ blog/src/components/LoginForm/LoginForm.js
@@ -46,7 +46,7 @@ const LoginForm = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:8080/auth/login', {
+      const response = await fetch('https://shopkubson.herokuapp.com/auth/login', {
         method: 'POST',
         headers: {
           'content-type': 'application/json',
